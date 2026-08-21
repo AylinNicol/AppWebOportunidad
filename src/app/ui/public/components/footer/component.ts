35d8c2ba02cc6708ebe5e728c68d89b7826b1aa8
Aylin :3 <aylinnicolninahannover@gmail.com>
2026-08-21 08:25:23 -0400

    Iconos

--- src/app/ui/public/components/footer/component.ts
+++ src/app/ui/public/components/footer/component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'footer-public',
   templateUrl: './component.html',
   styleUrls: ['./component.css'],
-  imports: [RouterLink, MatIcon],
+  imports: [RouterLink],
 })
 export class FooterComponent {}
